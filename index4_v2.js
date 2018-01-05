4586f5f64839ce96123ba8ca127426215d3d9a58
Mary Chew <jiayi1172@gmail.com>
2018-01-05 15:01:14 +0800

    Update index4_v2.js

--- index4_v2.js
+++ index4_v2.js
@@ -1,13 +1,3 @@
-$(document).ready(function(){
-
-  var content = "My New Awesome Content";
-
-  $(".panel-button").on("click",function(){
-    var panelId = $(this).attr("data-panelid");
-    $("#"+panelId).toggle();
-    $("#"+panelId+" .card-body").html(content);
-  });
-});
 
 /*
 $(document).ready(function(){
